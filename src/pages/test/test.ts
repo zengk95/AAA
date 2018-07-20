2aea9d8d2fef1700f4b45494fb2c9a47219fb6f5
Kerry <zengk95@gmail.com>
2018-07-19 22:49:24 -0500

    final version

--- src/pages/test/test.ts
+++ src/pages/test/test.ts
@@ -81,6 +81,24 @@ export class TestPage {
 
       var interval = setInterval(updateClock, 1000);
 
+      $(function() {
+        var taint, d, x, y;
+        $(".material-button").click(function(e){
+          if ($(this).find(".taint").length == 0) {
+            $(this).prepend("<span class='taint'></span>")
+          }
+          taint = $(this).find(".taint");
+          taint.removeClass("drop");
+          if(!taint.height() && !taint.width()) {
+            d = Math.max($(this).outerWidth(), $(this).outerHeight());
+          taint.css({height: d, width: d});
+          }
+          x = e.pageX - $(this).offset().left - taint.width()/2;
+          y = e.pageY - $(this).offset().top - taint.height()/2;
+          taint.css({top: y+'px', left: x+'px'}).addClass("drop");
+        });
+      });
+
     });//EOF DOCUMENT.READY
   }
 
